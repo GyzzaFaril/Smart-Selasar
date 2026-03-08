41b108e1141efc2aae984e86997fefd507f4e99e
Gyzza Faril <angkerbaker@gmail.com>
2026-03-08 10:18:46 +0700

    Menghapus variabel yang dobel

--- selasar-frontend/app/page.tsx
+++ selasar-frontend/app/page.tsx
@@ -3,9 +3,9 @@
 import { useEffect, useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function Home() {
   const [seats, setSeats] = useState([]);
